fdd9edadf78bdbb2d82b0d478798a1e198577589
stevelee5 <triamond3@gmail.com>
2015-07-06 07:58:26 -0400

    Test_0
    Test_0

---
+++ testApp WatchKit Extension/InterfaceController.h
@@ -0,0 +1,14 @@
+//
+//  InterfaceController.h
+//  testApp WatchKit Extension
+//
+//  Created by Steve Lee on 6/13/15.
+//  Copyright (c) 2015 Steve Lee. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface InterfaceController : WKInterfaceController
+
+@end
